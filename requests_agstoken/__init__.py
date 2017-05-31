a5a78e1a59cdeaf12f2dd8a0c25ac7cd252d2239
Patrick Foppe <pfoppe@blm.gov>
2017-05-30 20:35:16 -0600

    fixed __init__

--- requests_agstoken/__init__.py
+++ requests_agstoken/__init__.py
@@ -2,4 +2,4 @@
 from .requests_agstoken import ArcGISServerTokenAuth,ArcGISServerAuth
 from .agsexceptions import TokenAuthenticationError,TokenAuthenticationWarning
 
-__all__ = ('ArcGISServerTokenAuth','TokenAuthenticationError','TokenAuthenticationWarning')
+__all__ = ('ArcGISServerTokenAuth','ArcGISServerAuth','TokenAuthenticationError','TokenAuthenticationWarning')
